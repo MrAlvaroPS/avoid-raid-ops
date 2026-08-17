20de76149f176b928de69ef3cec57dd2945b5804
Alvaro Pompa Sanchez <31500250+MrAlvaroPS@users.noreply.github.com>
2026-08-17 23:39:14 +0200

    chore(release): sync product baseline to v3.9.4

--- packages/release/src/index.js
+++ packages/release/src/index.js
@@ -1,4 +1,4 @@
-export const PRODUCT_RELEASE_VERSION='3.9.2';
+export const PRODUCT_RELEASE_VERSION='3.9.4';
 export const PRODUCT_RELEASE_LABEL=`v${PRODUCT_RELEASE_VERSION}`;
 export const PRODUCT_RELEASE=Object.freeze({
   schema:'product-release-v1',
