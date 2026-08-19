b7dfbf13cca4b7c38ac3403960692528935a39a8
Alvaro Pompa Sanchez <31500250+MrAlvaroPS@users.noreply.github.com>
2026-08-20 00:50:43 +0200

    chore(loot): load v3.9.13.4 runtime

--- public/loot-runtime-v39132.js
+++ public/loot-runtime-v39132.js
@@ -1,10 +1,10 @@
 (()=>{
 'use strict';
-if(window.__AVOID_LOOT_V39133_LOADER__)return;
-window.__AVOID_LOOT_V39133_LOADER__=true;
+if(window.__AVOID_LOOT_V39134_LOADER__)return;
+window.__AVOID_LOOT_V39134_LOADER__=true;
 const script=document.createElement('script');
-script.src='/loot-runtime-v39133.js?v=3.9.13.3';
+script.src='/loot-runtime-v39134.js?v=3.9.13.4';
 script.async=false;
-script.dataset.avoidLootRuntime='3.9.13.3';
+script.dataset.avoidLootRuntime='3.9.13.4';
 document.head.append(script);
 })();
